7d386ae49dacb18ca164cbbf0562a816ecadd89b
Esteborg01 <egonzalez7208@gmail.com>
2025-12-05 14:22:15 -0600

    Primer commit del backend Esteborg
    Primer commit del backend Esteborg

---
+++ Index.js
@@ -0,0 +1,24 @@
+﻿import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 4000;
+
+
+app.get("/", (req, res) => {
+  res.send("Servidor Esteborg está vivo 🦾");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Esteborg backend escuchando en http://localhost:${PORT}`);
+});
